8c9319bff510bb4cd34b44d77374ef75445c215f
manish23 <manish.181623@ncit.edu.np>
2023-06-06 21:22:31 +0545

    changed views

--- healthapp/views.py
+++ healthapp/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import doctor
 from .models import consultationform
 from math import ceil
-from django.core.mail import EmailMessage,message
+from django.core.mail import EmailMessage, message
 from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,  login, logout
+from django.template.loader import render_to_string
 
 
 # Create your views here.
@@ -32,35 +36,112 @@ def ourdoctors(request):
 
 
 def Consultationform(request):
-    if request.method=='POST':
-        name = request.POST.get('name','')
-        email = request.POST.get('email','')
-        phone = request.POST.get('phone','')
-        department = request.POST.get('department','')
-        date = request.POST.get('date','')
-        time = request.POST.get('time','')
-        city = request.POST.get('city','')
-        state = request.POST.get('state','')
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        department = request.POST.get('department', '')
+        date = request.POST.get('date', '')
+        time = request.POST.get('time', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
         # print(name,email,phone,department,date,time,city,state)
-        Consultationform=consultationform(name=name,email=email,phone=phone,department=department,date=date,time=time,city=city,state=state)
+        Consultationform = consultationform(
+            name=name, email=email, phone=phone, department=department, date=date, time=time, city=city, state=state)
         Consultationform.save()
         # return HttpResponse('Thank you for filling appointment.We will reach you ASAP')
-        email=EmailMessage (
-         subject="Confirmation mail",
-         body="Thank you for filling appointment.We will reach you ASAP",
-         from_email=settings.EMAIL_HOST_USER,
-         to=[email]
+        email = EmailMessage(
+            subject="Confirmation mail",
+            body="Thank you for filling appointment.We will reach you ASAP",
+            from_email=settings.EMAIL_HOST_USER,
+            to=[email]
         )
         email.send()
 
-    return render(request, 'healthapp/consultationform.html')
+    return redirect("/")
 
 
 def blog(request):
     return render(request, 'healthapp/blog.html')
 
-def docview(request,myid):
-    # Fetching using id
-    doc= doctor.objects.filter(id=myid)
 
-    return render(request, 'healthapp/docview.html',{'doctors':doc[0]})
+def docview(request, myid):
+    # Fetching using id
+    doc = doctor.objects.filter(id=myid)
+
+    return render(request, 'healthapp/docview.html', {'doctors': doc[0]})
+
+
+#  SIGNUP SECTION
+def handleSignUp(request):
+    if request.method == "POST":
+        # Get the post parameters
+        username = request.POST['username']
+        email = request.POST['email']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        # check for errorneous input
+        if len(username) > 10:
+            messages.warning(request," Your user name must be under 10 characters")
+            return redirect("/")
+
+        if not username.isalnum():
+            messages.warning( request," User name should only contain letters and numbers")
+            return redirect("/")
+        if (pass1 != pass2):
+            messages.warning(request," Passwords do not match")
+            return redirect("/")
+        if User.objects.filter (username=username).exists():
+            messages.warning(request," User name already exists")
+            return redirect("/")
+
+        # Create the user
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        # end of create user 
+        mydict = {'username': username}
+        myuser.save()
+        messages.success(request, " User has been successfully created")
+        html_template='healthapp/email_template.html'
+        html_message=render_to_string(html_template,context=mydict)
+        subject='Welcome to HealthMate'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [email]
+        message = EmailMessage(subject, html_message, email_from, recipient_list)
+        message.content_subtype = 'html'
+        message.send()
+        return redirect("/")
+
+    else:
+        return HttpResponse("404 - Not found")
+
+    # Login section
+
+
+def handleLogin(request):
+    if request.method == "POST":
+        # Get the post parameters
+        loginusername = request.POST['loginusername']
+        loginemail = request.POST['loginemail']
+        loginpassword = request.POST['loginpassword']
+
+        user = authenticate(username=loginusername,email=loginemail, password=loginpassword)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In")
+            return redirect("/")
+        else:
+            messages.error(request, "Invalid credentials! Please try again")
+            return redirect("/")
+
+    return HttpResponse("404- Not found")
+
+
+def handelLogout(request):
+    logout(request)
+    messages.success(request, "Successfully logged out")
+    return redirect("/")
